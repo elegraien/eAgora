9d807bdd4f340e9cfb435a83de9d41d3cf1d0364
christiannavelot <christian.navelot@gmail.com>
2018-01-08 14:08:12 +0100

    Ajout de la solution à Github

---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
---
+++ src/Web/ViewModels/Manage/ExternalLoginsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels.Manage
+{
+    public class ExternalLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationScheme> OtherLogins { get; set; }
+        public bool ShowRemoveButton { get; set; }
+        public string StatusMessage { get; set; }
+    }
+
+
+}
